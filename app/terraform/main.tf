866227ac65fb36ff341381d00a5d02c676fd5cf4
Mehrazulx <mehrazul@gmail.com>
2026-03-16 17:40:05 +0000

    Remove duplicate outputs from main.tf

--- app/terraform/main.tf
+++ app/terraform/main.tf
@@ -297,15 +297,3 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-# Outputs
-output "load_balancer_url" {
-  value = "http://${aws_lb.main.dns_name}"
-}
-
-output "cluster_name" {
-  value = aws_ecs_cluster.main.name
-}
-
-output "service_name" {
-  value = aws_ecs_service.app.name
-}
